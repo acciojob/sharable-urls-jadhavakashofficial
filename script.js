d02f057cec76698d0c7b5b88027fd13e97e7a642
Akash Jadhav <102428182+jadhavakashofficial@users.noreply.github.com>
2025-03-10 15:21:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// your code here
+document.getElementById('button').addEventListener('click', function (e) {
+  e.preventDefault(); 
+
+  const nameValue = document.getElementById('name').value.trim();
+  const yearValue = document.getElementById('year').value.trim();
+
+  let baseUrl = "https://localhost:8080/";
+  const params = [];
+
+
+  if (nameValue) {
+    params.push(`name=${encodeURIComponent(nameValue)}`);
+  }
+
+  if (yearValue) {
+    params.push(`year=${encodeURIComponent(yearValue)}`);
+  }
+
+  if (params.length > 0) {
+    baseUrl += "?" + params.join("&");
+  }
+
+  document.getElementById("url").textContent = baseUrl;
+});
